2609e97808abeaabc182d2d78be2ba0d25387d91
ManmadhaKuttum <manmadhakuttum685@gmail.com>
2025-12-13 15:34:58 +0000

    Lab-2: enable ptrace tracing for child process

--- lab2/mini-debugger/debugger.c
+++ lab2/mini-debugger/debugger.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/ptrace.h>
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -12,6 +14,7 @@ int main(int argc, char *argv[]) {
     pid_t pid = fork();
 
     if (pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execl(argv[1], argv[1], NULL);
     } else {
         int status;
