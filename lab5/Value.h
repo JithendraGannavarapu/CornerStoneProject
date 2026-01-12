6015ae952e4c5ad108b911f8e11780fed34014a4
Jithendra Gannavarapu <jithendra@Jithendras-MacBook-Air.local>
2026-01-13 00:30:18 +0530

    Defining the objects and integers values

--- lab5/Value.h
+++ lab5/Value.h
@@ -12,19 +12,16 @@ struct Value {
         int32_t asInt;
         Object* asObj;
     };
-    
-    // Constructors for safety
+
     Value() : type(VAL_INT), asInt(0) {}
     Value(ValueType t) : type(t), asInt(0) {}
 };
 
-// Macros for easy usage
 #define IS_INT(v) ((v).type == VAL_INT)
 #define IS_OBJ(v) ((v).type == VAL_OBJ)
 #define AS_INT(v) ((v).asInt)
 #define AS_OBJ(v) ((v).asObj)
 
-// Helpers to create values
 inline Value INT_VAL(int32_t val) {
     Value v(VAL_INT); v.asInt = val; return v;
 }
